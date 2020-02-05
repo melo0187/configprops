14ea15296aa0a92855610374b337e447a05c816a
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2020-02-05 11:26:05 +0100

    Drop obsolete import

--- src/test/kotlin/com/meisterplan/example/configprops/DemoConfigTest.kt
+++ src/test/kotlin/com/meisterplan/example/configprops/DemoConfigTest.kt
@@ -1,8 +1,6 @@
 package com.meisterplan.example.configprops
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
-
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
